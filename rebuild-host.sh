9da55df274f1c24750ba20ca99e587acb46b6cb2
Laurent Itti <jevois.org@gmail.com>
2021-09-30 14:59:45 -0700

    now using differentiated share-jevois and share-jevoispro

--- rebuild-host.sh
+++ rebuild-host.sh
@@ -1,12 +1,8 @@
 #!/bin/sh
 
-# On ARM hosts like Raspberry Pi3, we will likely run out of memory if attempting more than 1 compilation thread:
-ncpu=`cat /proc/cpuinfo |grep processor|wc -l`
-if [ `cat /proc/cpuinfo | grep ARM | wc -l` -gt 0 ]; then ncpu=1; fi
-
 /bin/rm -rf hbuild \
     && mkdir hbuild \
     && cd hbuild \
     && cmake "$@" .. \
-    && make -j ${ncpu} \
+    && make -j \
     && sudo make install
--- rebuild-pro-host.sh
+++ rebuild-pro-host.sh
@@ -1,13 +1,9 @@
 #!/bin/sh
 
-# On JeVoisPro, limit the number of compile threads to not run out of memory:
-ncpu=`grep -c processor /proc/cpuinfo`
-if [ `grep -c JeVois /proc/cpuinfo` -gt 0 ]; then ncpu=4; fi
-
 # Build everything:
 sudo /bin/rm -rf phbuild \
     && mkdir phbuild \
     && cd phbuild \
     && cmake "$@" -DJEVOIS_HARDWARE=PRO .. \
-    && make -j ${ncpu} \
+    && make -j \
     && sudo make install
