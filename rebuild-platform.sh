bda47352ef35a7d0c6c647e9c5545d93a333eeb9
Laurent Itti <jevois.org@gmail.com>
2024-06-12 15:37:47 -0700

    build script update

--- rebuild-platform.sh
+++ rebuild-platform.sh
@@ -8,9 +8,6 @@ set -e
 create_jvpkg="no"
 if [ "X$1" = "X--jvpkg" ]; then create_jvpkg="yes"; shift; fi
 
-# Get the external contributed packages if they are not here or are outdated:
-./Contrib/check.sh
-
 # Let's build it:
 sudo /bin/rm -rf pbuild
 mkdir pbuild
@@ -20,4 +17,4 @@ make -j
 sudo make install
 cd ..
 
-if [ $jvpkg = "yes" ]; then jevois-jvpkg `cat pbuild/jvpkg-args`; fi
+if [ $create_jvpkg = "yes" ]; then jevois-jvpkg `cat pbuild/jvpkg-args`; fi
