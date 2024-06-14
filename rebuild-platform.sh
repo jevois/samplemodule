e68683350f88b92e213004d9c0fc605eda814d9c
Laurent Itti <jevois.org@gmail.com>
2024-06-14 14:47:49 -0700

    simplifying the platform build process for jevois-a33

--- rebuild-platform.sh
+++ rebuild-platform.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
-# USAGE: rebuild-platform.sh [--jvpkg] [cmake opts]
-#
-# If --jvpkg is specified, then a jevois .jvpkg package will also be created
+# USAGE: rebuild-platform.sh [cmake opts]
 
 set -e
-
-create_jvpkg="no"
-if [ "X$1" = "X--jvpkg" ]; then create_jvpkg="yes"; shift; fi
 
 # Let's build it:
 sudo /bin/rm -rf pbuild
@@ -17,4 +12,4 @@ make -j
 sudo make install
 cd ..
 
-if [ $create_jvpkg = "yes" ]; then jevois-jvpkg `cat pbuild/jvpkg-args`; fi
+jevois-jvpkg `cat pbuild/jvpkg-args`
