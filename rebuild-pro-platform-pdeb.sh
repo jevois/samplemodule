788b767671371e8e90a7dbcca3ae65f2620d22f3
Laurent Itti <jevois.org@gmail.com>
2024-07-11 16:31:05 -0700

    no more staging defs

--- rebuild-pro-platform-pdeb.sh
+++ rebuild-pro-platform-pdeb.sh
@@ -5,7 +5,7 @@
 sudo /bin/rm -rf ppdbuild \
     && mkdir ppdbuild \
     && cd ppdbuild \
-    && cmake -DJEVOIS_HARDWARE=PRO -DJEVOIS_PLATFORM=ON -DJEVOIS_MODULES_TO_STAGING=ON -DJEVOISPRO_PLATFORM_DEB=ON $@ .. \
+    && cmake -DJEVOIS_HARDWARE=PRO -DJEVOIS_PLATFORM=ON -DJEVOISPRO_PLATFORM_DEB=ON $@ .. \
     && make -j \
     && sudo make install \
     && sudo cpack
